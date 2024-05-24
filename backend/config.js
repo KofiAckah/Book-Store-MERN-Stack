ca4ce01d0a29c94c2df5d9fe87cf9dac692ce7da
Livingstone Ackah <kofiackah360@gmail.com>
2024-05-24 00:59:14 +0000

    Initial Commit

---
+++ backend/config.js
@@ -0,0 +1,6 @@
+export const PORT = 3004;
+
+export const mongoDBURL =
+  "mongodb+srv://KofiAckah:password1234@book-store-mern.dfgil92.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Book-Store-MERN";
+
+// HvUYSgfQjuGUuX7U oAPqfdQbR5fDln60
